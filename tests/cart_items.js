25889764fb631463fd99e3a94d19c786a0240404
MiaLearning <black_flyleaf@yahoo.com>
2023-02-10 18:09:05 +0200

    Add 2 products to cart and remove one; Verify social buttons - Twitter

--- tests/cart_items.js
+++ tests/cart_items.js
@@ -2,11 +2,18 @@ import { Selector, t, ClientFunction } from "testcafe";
 
 import { baseUrl, loginButton, validUserName, validPassword,
         userNameInput, passwordInput, } from "./login.js";
-const backpackAddToCart = Selector('#add-to-cart-sauce-labs-backpack');
-const backpackRemoveFromCart = Selector("#remove-sauce-labs-backpack");
+const firstProductAddToCart = Selector('#add-to-cart-sauce-labs-backpack');
+const firstProductRemoveFromCart = Selector("#remove-sauce-labs-backpack");
+const secondProductAddToCart = Selector('#add-to-cart-sauce-labs-bike-light');
+const secondProductRemoveFromCart = Selector('#remove-sauce-labs-bike-light');
 const shoppingCartCount = Selector('.shopping_cart_badge');
 const shoppingCartButton = Selector('.shopping_cart_link');
 const cartItem = Selector('.cart_item');
+const cartQuantity = Selector('.cart_quantity');
+const twitterButton = Selector('.social_twitter');
+const facebookButton = Selector('.social_facebook ');
+const linkedinButton = Selector('.social_linkedin');
+const getURL = ClientFunction(() => window.location.href);
 
 
 
@@ -19,28 +26,60 @@ test(`Add one item to cart`, async t => {
         .typeText(userNameInput, validUserName)
         .typeText(passwordInput, validPassword)
         .click(loginButton)
-        .click(backpackAddToCartButton)
+        .click(firstProductAddToCart)
 
-    const shoppingCart = Selector('.shopping_cart_link').exists;
-    const cartNumber = shoppingCartCount.innerText;
+    const cartNumber = await shoppingCartCount.innerText;
 
-    await t.expect(shoppingCart).ok();
     await t.expect(cartNumber).eql('1');
 });
 
 
 test(`Remove product from cart`, async t => {
     await t
+        .setTestSpeed(0.8)
         .typeText(userNameInput, validUserName)
         .typeText(passwordInput, validPassword)
         .click(loginButton)
-        .click(backpackAddToCart)
-        .wait(1000)
+        .click(firstProductAddToCart)
         .click(shoppingCartButton)
-        .click(backpackRemoveFromCart)
-        .wait(1000)
+        .click(firstProductRemoveFromCart)
 
     const isCartItemVisible = await cartItem.visible;
+    const isProductInCart = await shoppingCartCount.exists;
 
     await t.expect(isCartItemVisible).eql(false)
+    await t.expect(isProductInCart).eql(false)
+});
+
+
+test(`Add two products to cart and remove one`, async t => {
+    await t 
+         .setTestSpeed(0.8)
+         .typeText(userNameInput, validUserName)
+         .typeText(passwordInput, validPassword)
+         .click(loginButton)
+         .click(firstProductAddToCart)
+         .click(secondProductAddToCart)
+         .click(shoppingCartButton)
+         .click(secondProductRemoveFromCart)
+
+    const cartNumber = await shoppingCartCount.innerText;
+    const cartListProductNo = await cartQuantity.innerText;
+
+    await t.expect(cartNumber).eql('1');
+    await t.expect(cartListProductNo).eql('1'); 
+});
+
+
+test(`Verify social buttons - Twitter`, async t => {
+    await t
+        .setTestSpeed(0.8)
+        .typeText(userNameInput, validUserName)
+        .typeText(passwordInput, validPassword)
+        .click(loginButton)
+        .click(twitterButton)
+    
+    const twitterPage = await getURL();
+
+    await t.expect(twitterPage).eql('https://twitter.com/saucelabs');
 });
